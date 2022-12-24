7064fdd9e5dd3ffa8d5e4fabb6328ddd6f73fad9
habasm <habasm365@gmail.com>
2022-12-24 22:37:37 +0300

    errors fixed of the variable

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import './style.css';
 import AddScores from './views/AddScore.js';
 import Footer from './views/Footer.js';
 import Modal from './views/Modal.js';
-import RecentScores from './views/RecentScores.js';
+import recentScores from './views/RecentScores.js';
 
 const leaderContent = document.querySelector('.leaderboard-content');
 
 // update leaderboard content
-leaderContent.innerHTML = _ + RecentScores() + AddScores();
+leaderContent.innerHTML = _ + recentScores() + AddScores();
 
 // update body
 document.body.innerHTML += Modal() + Footer();
--- src/models/Utils.js
+++
@@ -1,6 +0,0 @@
-const BaseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-const contentType = { 'content-type': 'application/json' };
-
-export {
-  BaseUrl, contentType,
-};
